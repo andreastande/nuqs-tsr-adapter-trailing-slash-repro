73e280390cd5323b1a5a8ad0cacc6a4d300df2a5
Andreas Tande <andretan@stud.ntnu.no>
2025-11-15 05:24:03 +0100

    remove Tanstack Devtools

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,4 @@
-import { TanStackDevtools } from "@tanstack/react-devtools"
 import { createRootRoute, Outlet } from "@tanstack/react-router"
-import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools"
 import { NuqsAdapter } from "nuqs/adapters/tanstack-router"
 
 export const Route = createRootRoute({
@@ -9,17 +7,6 @@ export const Route = createRootRoute({
       <NuqsAdapter>
         <Outlet />
       </NuqsAdapter>
-      <TanStackDevtools
-        config={{
-          position: "bottom-left",
-        }}
-        plugins={[
-          {
-            name: "Tanstack Router",
-            render: <TanStackRouterDevtoolsPanel />,
-          },
-        ]}
-      />
     </>
   ),
 })
